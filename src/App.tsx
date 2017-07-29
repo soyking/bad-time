c8dc8296e498640f96bd64f87e9c4653a0932854
soyking <soyking@outlook.com>
2017-07-29 17:40:49 +0800

    init app

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+
+class App extends React.Component<{}, {}> {
+  render() {
+    return (
+      <div>
+        Really Bad
+      </div>
+    );
+  }
+}
+
+export default App;
