b0acadf3338646c5ceaae01a6ef8e1f1e936abce
soyking <soyking@outlook.com>
2017-07-31 00:12:10 +0800

    DateCell Component

--- src/App.tsx
+++ src/App.tsx
@@ -1,47 +1,39 @@
-import * as React from 'react';
-import Calendar from 'rc-calendar';
-import 'rc-calendar/assets/index.css';
-import { Box } from 'react-polymer-layout';
+import * as React from 'react'
+import Calendar from 'rc-calendar'
+import 'rc-calendar/assets/index.css'
+import { Box } from 'react-polymer-layout'
+import DateCell from './DateCell'
 
 const levelColor = {
-  1: "#FFFF99",
-  2: "#FF6633",
+  1: '#FFFF99',
+  2: '#FF6633',
 }
 
 class App extends React.Component<{}, {}> {
   _dateRender(current, value) {
-    window["state"] = this.state
-    let color = levelColor[this.state["level"][current.format("YYYY-MM-DD")]] || ""
-    window["current"] = current
-    return (
-      <div key={current.format('MMMM Do YYYY, h:mm:ss a')}
-        className='rc-calendar-date'
-        style={{ backgroundColor: color }}
-      >
-        {current.date()}
-      </div>
-    )
+    let color = levelColor[this.state['level'][current.format('YYYY-MM-DD')]] || ''
+    let currentMonth = current.month() === value.month()
+    return <DateCell date={current} color={color} currentMonth={currentMonth} />
   }
 
   _getLevel() {
     return {
-      "2017-08-04": 1,
-      "2017-08-03": 1,
-      "2017-08-02": 2,
-      "2017-08-01": 2,
-      "2017-07-31": 1,
-      "2017-07-30": 1,
-      "2017-07-29": 2,
-      "2017-07-28": 1,
-      "2017-07-27": 2,
-      "2017-07-26": 2,
+      '2017-08-04': 1,
+      '2017-08-03': 1,
+      '2017-08-02': 2,
+      '2017-08-01': 2,
+      '2017-07-31': 1,
+      '2017-07-30': 1,
+      '2017-07-29': 2,
+      '2017-07-28': 1,
+      '2017-07-27': 2,
+      '2017-07-26': 2,
     }
   }
 
   constructor(props: any) {
-    super(props);
-    console.log("constructor")
-    this.state = { "level": this._getLevel() }
+    super(props)
+    this.state = { 'level': this._getLevel() }
   }
 
 
@@ -51,11 +43,14 @@ class App extends React.Component<{}, {}> {
         <Calendar
           style={{ width: 500 }}
           onSelect={() => { console.log('select') }}
+          onChange={() => { console.log('change') }}
           dateRender={this._dateRender.bind(this)}
+          renderFooter={() => { return <div>footer</div> }}
+          showToday={false}
         />
       </Box>
     );
   }
 }
 
-export default App;
+export default App
---
+++ src/DateCell.tsx
@@ -0,0 +1,34 @@
+
+import { Box } from 'react-polymer-layout'
+import * as React from 'react'
+import { Moment } from 'moment'
+
+export interface Props {
+    date: Moment
+    color: string
+    currentMonth: boolean
+}
+
+export default class DateCell extends React.Component<Props, {}> {
+    render() {
+        let { date, color, currentMonth } = this.props
+        let fontColor = currentMonth ? '#000' : '#aaa'
+        return (
+            <Box centerJustified>
+                <Box center centerJustified style={{
+                    width: 26,
+                    height: 26,
+                    backgroundColor: color
+                }}>
+                    <div key={date.format('MMMM Do YYYY, h:mm:ss')}
+                        style={{
+                            color: fontColor
+                        }}
+                    >
+                        {date.date()}
+                    </div>
+                </Box>
+            </Box>
+        )
+    }
+}
