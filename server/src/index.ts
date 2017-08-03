196ad9d2c4b9ee552fdd0b609b85348a3061513c
soyking <soyking@outlook.com>
2017-08-04 01:27:38 +0800

    API /api/items

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,11 +1,16 @@
 import * as express from 'express'
 const app = express()
 
-app.get('/', function (req, res) {
-  res.send('Hello World!')
+app.get('/api/items', function (req, res) {
+  res.send([
+    'coke',
+    'coffee',
+    'ice-cream',
+    'eat-after-9'
+  ])
 })
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
+app.listen(3001, function () {
+  console.log('Example app listening on port 3001!')
 })
 
